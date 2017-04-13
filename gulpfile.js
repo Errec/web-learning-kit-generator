f83383748bb0a119ab239793914e86c7c28c6588
Raniro Coelho <errecme@gmail.com>
2017-04-13 19:18:15 -0300

    Add gulp-footer to gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var beeper       = require('beeper');
 var browserSync  = require('browser-sync');
 var cache        = require('gulp-cache');
 var cleanCSS     = require('gulp-clean-css');
+var footer       = require('gulp-footer');
 var gconcat       = require('gulp-concat');
 var gulp         = require('gulp');
+var gutil        = require('gulp-util');
 var imagemin     = require('gulp-imagemin');
 var notify       = require('gulp-notify');
 var plumber      = require('gulp-plumber');
@@ -13,8 +15,7 @@ var rename       = require("gulp-rename");
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var uglify       = require('gulp-uglify');
-var gutil        = require('gulp-util');
-// sudo npm install gulp-uglify browser-sync gulp-plumber gulp-autoprefixer gulp-sass gulp-pug gulp-imagemin gulp-cache gulp-clean-css gulp-sourcemaps gulp-concat beeper gulp-util gulp-rename gulp-notify --save-dev
+// sudo npm install gulp-uglify browser-sync gulp-plumber gulp-autoprefixer gulp-sass gulp-pug gulp-imagemin gulp-cache gulp-clean-css gulp-sourcemaps gulp-concat beeper gulp-util gulp-rename gulp-notify gulp-footer --save-dev
 var jsVendorFiles = [];             // Holds the js vendor files to be concatenated
 var myJsFiles     = ['js/*.js'];    // Holds the js files to be concatenated
 var fs            = require('fs');  // ExistsSync var to check if font directory patch exist
@@ -27,7 +28,7 @@ var onError       = function(err) { // Custom error msg with beep sound and text
     this.emit('end');
     gutil.log(gutil.colors.red(err));
 };
-var bowerDest          = "js/vendor/";
+var bowerDest          = "/js/vendor/";
 var buildPath          = "build";
 var bootstrapPath      = "bootstrap/dist/js/bootstrap.min.js";
 var bootstrapFontsPath = "bootstrap/dist/fonts/**.*";
@@ -74,6 +75,7 @@ gulp.task('images', function() {
   .pipe(gulp.dest('build/img/'));
 });
 
+
 gulp.task('check-bower-vendor', function() {
   if (!fs.existsSync(bowerDest + bootstrapPath)) {
     gulp.src(bowerDest + bootstrapFontsPath)
@@ -82,6 +84,7 @@ gulp.task('check-bower-vendor', function() {
     .pipe(gulp.dest('build/js/vendor/bootstrap/dist/js/'));
     // jsVendorFiles.push("js/vendor/bootstrap/dist/js/bootstrap.min.js");
   }
+
   if (!fs.existsSync(bowerDest + jqueryPath)) {
     gulp.src(bowerDest + jqueryPath)
     .pipe(gulp.dest('build/js/vendor/jquery/dist/jquery/'));
