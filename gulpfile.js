a4d35cd7013911942bc5d9caf4737f26c6334097
Kamil Łopuszański <kamil.lopuszanski@gmail.com>
2024-01-18 00:00:10 -0300

    adjust for new versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
-var autoprefixer       = require('gulp-autoprefixer');
-var beeper             = require('beeper');
-var browserSync        = require('browser-sync');
-var cache              = require('gulp-cache');
-var cleanCSS           = require('gulp-clean-css');
-var gconcat            = require('gulp-concat');
-var gulp               = require('gulp');
-var gutil              = require('gulp-util');
-var imagemin           = require('gulp-imagemin');
-var notify             = require('gulp-notify');
-var plumber            = require('gulp-plumber');
-var pug                = require('gulp-pug');
-var rename             = require("gulp-rename");
-var sass               = require('gulp-sass');
-var sourcemaps         = require('gulp-sourcemaps');
-var uglify             = require('gulp-uglify');
+import autoprefixer from 'gulp-autoprefixer';
+import beeper from 'beeper';
+import sync from 'browser-sync';
+const { init, reload } = sync;
+import cache from 'gulp-cache';
+import cleanCSS from 'gulp-clean-css';
+import gconcat from 'gulp-concat';
+import gulp from 'gulp';
+const { src, dest, series, watch: gulpWatch } = gulp;
+import util from 'gulp-util';
+const { log, colors } = util;
+import imagemin from 'gulp-imagemin';
+import { onError as _onError } from 'gulp-notify';
+import plumber from 'gulp-plumber';
+import pug from 'gulp-pug';
+import rename from "gulp-rename";
+
+// gulp-sass 5 does not have a default Sass compiler; please set one yourself
+// https://stackoverflow.com/a/69066757/7769052
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+const sass = gulpSass(dartSass)
+
+import sourcemaps from 'gulp-sourcemaps';
+const { init: _init, write } = sourcemaps;
+import uglify from 'gulp-uglify';
 // sudo npm install gulp-uglify browser-sync gulp-plumber gulp-autoprefixer gulp-sass gulp-pug gulp-imagemin gulp-cache gulp-clean-css gulp-sourcemaps gulp-concat beeper gulp-util gulp-rename gulp-notify --save-dev
 var jsVendorFiles      = [];             // Holds the js vendor files to be concatenated
 var myJsFiles          = ['js/*.js'];    // Holds the js files to be concatenated
-var fs                 = require('fs');  // ExistsSync var to check if font directory patch exist
+import { existsSync, readFileSync, writeFile } from 'fs';  // ExistsSync var to check if font directory patch exist
 var bowerDirectory     = getBowerDirectory();
 var bootstrapJSPath    = bowerDirectory + "bootstrap/dist/js/bootstrap.min.js";
 var bootstrapCSSPath   = bowerDirectory + "bootstrap/dist/css/bootstrap.min.css";
@@ -25,21 +35,21 @@ var bootstrapFontsPath = bowerDirectory + "bootstrap/dist/fonts/**.*";
 var jqueryPath         = bowerDirectory + "jquery/dist/jquery.min.js";
 var bootstrapExist     = false;
 var onError            = function(err) { // Custom error msg with beep sound and text color
-    notify.onError({
+    _onError({
       title:    "Gulp error in " + err.plugin,
       message:  err.toString()
     })(err);
     beeper(3);
     this.emit('end');
-    gutil.log(gutil.colors.red(err));
+    log(colors.red(err));
 };
 
 function getBowerDirectory() {
   var bowerComponents = "./bower_components";
-  if(fs.existsSync('.bowerrc')) {
-    var bowerrc = JSON.parse(fs.readFileSync('.bowerrc').toString());
+  if(existsSync('.bowerrc')) {
+    var bowerrc = JSON.parse(readFileSync('.bowerrc').toString());
     return bowerrc.directory;
-  } else if (fs.existsSync(bowerComponents)) {
+  } else if (existsSync(bowerComponents)) {
     return bowerComponents + '/';
   } else {
     return '';
@@ -49,8 +59,8 @@ function getBowerDirectory() {
 function setupJquery(data) {
   var jqueryCDN = '    script(src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js" integrity="{{JQUERY_SRI_HASH}}" crossorigin="anonymous")';
   var jqueryLocalFallback = "    <script>window.jQuery || document.write(" + "'<script src=" + '"js/vendor/jquery/dist/jquery/jquery.min.js"' + "><\\/script>')</script>";
-  gulp.src(jqueryPath)
-  .pipe(gulp.dest('./build/js/vendor/jquery/dist/jquery'));
+  src(jqueryPath)
+  .pipe(dest('./build/js/vendor/jquery/dist/jquery'));
   data.splice(data.length, 0, jqueryCDN);
   data.splice(data.length, 0, jqueryLocalFallback);
 }
@@ -62,12 +72,12 @@ function setupBootstrap(data) {
   var bootstrapCSSLocalFallback = '    div(id="bootstrapCssTest" class="hidden")\n' + "    <script>$(function(){if ($('#bootstrapCssTest').is(':visible')){$('head').prepend('<link rel=" + '"stylesheet" href="/js/vendor/bootstrap/dist/css/bootstrap.min.css">' + "');}});</script>";
   var bootstrapJSCDN = '    script(src="https://maxcdn.bootstrapcdn.com/bootstrap/{{BOOTSTRAP_VERSION}}/js/bootstrap.min.js", integrity="{{BOOTSTRAP_SRI_HASH}}", crossorigin="anonymous")';
   var bootstrapJSLocalFallback = "    <script>if(typeof($.fn.modal) === 'undefined'" + ") {document.write('<script src=" + '"/js/vendor/bootstrap/dist/js/bootstrap.min.js"' + "><\\/script>')}</script>";
-  gulp.src(bootstrapFontsPath)
-  .pipe(gulp.dest('./build/js/vendor/bootstrap/dist/fonts'));
-  gulp.src(bootstrapJSPath)
-  .pipe(gulp.dest('./build/js/vendor/bootstrap/dist/js'));
-  gulp.src(bootstrapCSSPath)
-  .pipe(gulp.dest('./build/js/vendor/bootstrap/dist/css'));
+  src(bootstrapFontsPath)
+  .pipe(dest('./build/js/vendor/bootstrap/dist/fonts'));
+  src(bootstrapJSPath)
+  .pipe(dest('./build/js/vendor/bootstrap/dist/js'));
+  src(bootstrapCSSPath)
+  .pipe(dest('./build/js/vendor/bootstrap/dist/css'));
 
   data.splice(8, 0, bootstrapCSSCDN);
   data.splice(data.length, 0, bootstrapJSCDN);
@@ -84,49 +94,56 @@ function findKeyText(data, txt) {
   return false;
 }
 
-gulp.task('styles', function() {
-  gulp.src('styles/*.scss')
+function styles(done) {
+  src('styles/*.scss')
   .pipe(plumber({ errorHandler: onError }))
-  .pipe(sourcemaps.init())
+  .pipe(_init())
   .pipe(sass({indentedSyntax: true}))
   .pipe(autoprefixer({
-    browsers: ['last 5 versions'],
     cascade: false}))
   .pipe(cleanCSS())
-  .pipe(sourcemaps.write())
+  .pipe(write())
   .pipe(rename({ suffix: '.min'}))
-  .pipe(gulp.dest('build/css'));
-});
+  .pipe(dest('build/css'));
 
-gulp.task('templates', function() {
-  gulp.src('./*.pug')
+  done();
+}
+
+function templates(done) {
+  src('./*.pug')
   .pipe(plumber({ errorHandler: onError }))
   .pipe(pug())
-  .pipe(gulp.dest('build/'));
-});
+  .pipe(dest('build/'));
 
-gulp.task('scripts', function() {
-  return gulp.src(myJsFiles.concat(jsVendorFiles))
+  done();
+}
+
+function scripts(done) {
+  return src(myJsFiles.concat(jsVendorFiles))
   .pipe(plumber({ errorHandler: onError }))
-  .pipe(sourcemaps.init())
+  .pipe(_init())
   .pipe(gconcat('bundle.js'))
   .pipe(uglify())
-  .pipe(sourcemaps.write())
+  .pipe(write())
   .pipe(rename({ suffix: '.min'}))
-  .pipe(gulp.dest('build/js'));
-});
+  .pipe(dest('build/js'));
 
-gulp.task('images', function() {
-  gulp.src('img/**/*')
+  done();
+}
+
+function images(done) {
+  src('img/**/*')
   .pipe(cache(imagemin({
     optimizationLevel: 3,
     progressive: true,
     interlaced: true})))
-  .pipe(gulp.dest('build/img/'));
-});
+  .pipe(dest('build/img/'));
 
-gulp.task('setup-src', function() {
-  var data = fs.readFileSync('./index.pug').toString().split("\n");
+  done();
+}
+
+function setupSrc(done) {
+  var data = readFileSync('./index.pug').toString().split("\n");
 
   if(data[data.length - 1] === '') {
     data.pop();
@@ -137,11 +154,11 @@ gulp.task('setup-src', function() {
   }
 
   if(bowerDirectory) {
-    if(fs.existsSync(bootstrapJSPath) && !findKeyText(data, 'bootstrap.min.css')) {
+    if(existsSync(bootstrapJSPath) && !findKeyText(data, 'bootstrap.min.css')) {
       setupBootstrap(data);
     }
 
-    if(fs.existsSync(jqueryPath) && !bootstrapExist  && !findKeyText(data, 'jquery.min.js')) {
+    if(existsSync(jqueryPath) && !bootstrapExist  && !findKeyText(data, 'jquery.min.js')) {
       setupJquery(data);
     }
   }
@@ -151,32 +168,28 @@ gulp.task('setup-src', function() {
   }
 
   var text = data.join("\n");
-  fs.writeFile('./index.pug', text, function (err) {
+  writeFile('./index.pug', text, function (err) {
     if (err) throw err;
   });
-});
 
-gulp.task('default', function() {
-  console.log("Use 'gulp setup' command to initialize the project files");
-});
+  done();
+}
 
-gulp.task('setup', function() {
-  gulp.start('styles', 'templates', 'scripts', 'images', 'setup-src');
-});
+function watchTask() {
+  gulpWatch('styles/**/*', styles);
+  gulpWatch(['templates/**/*', './*.pug'], templates);
+  gulpWatch('js/*.js', scripts);
+  gulpWatch('img/**/*', images);
 
-gulp.task('watch', function() {
-  gulp.watch('styles/**/*',                        ['styles']);
-  gulp.watch(['templates/**/*', './*.pug'],        ['templates']);
-  gulp.watch('js/*.js',                            ['scripts']);
-  gulp.watch('img/**/*',                           ['images']);
+    init.init({
+      server: {
+        baseDir: "build/"
+      }
+    });
 
-// init server
-  browserSync.init({
-    server: {
-      proxy: "local.build",
-      baseDir: "build/"
-    }
-  });
+  gulpWatch(['build/**'], reload);
+}
 
-  gulp.watch(['build/**'], browserSync.reload);
-});
+// The 'setup' task that runs all the tasks in series
+export const setup = series(styles, templates, scripts, images, setupSrc);
+export const watch = series(watchTask);
