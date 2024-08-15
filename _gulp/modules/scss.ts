d1a4db016b651c8dc2915283d5d095440671d170
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-15 18:42:04 -0300

    Add gulp task for compiling SCSS files

---
+++ _gulp/modules/scss.ts
@@ -0,0 +1,19 @@
+import { dest, src } from 'gulp';
+import autoprefixer from 'gulp-autoprefixer';
+import cssnano from 'gulp-cssnano';
+import plumber from 'gulp-plumber';
+import gulpSass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import sass from 'sass';
+
+const sassCompiler = gulpSass(sass);
+
+export function styleTask() {
+  return src('src/scss/main.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sassCompiler())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/css'));
+}
