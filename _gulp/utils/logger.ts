a63998b75cbdf250882b9d59b6bfbd71b5449a57
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-15 18:48:33 -0300

    Add logger utility for logging messages with different levels

---
+++ _gulp/utils/logger.ts
@@ -0,0 +1,6 @@
+export const logger = {
+    info: (message: string): void => console.log(`\x1b[36m[INFO]\x1b[0m ${message}`),
+    success: (message: string): void => console.log(`\x1b[32m[SUCCESS]\x1b[0m ${message}`),
+    warning: (message: string): void => console.log(`\x1b[33m[WARNING]\x1b[0m ${message}`),
+    error: (message: string): void => console.log(`\x1b[31m[ERROR]\x1b[0m ${message}`),
+  };
