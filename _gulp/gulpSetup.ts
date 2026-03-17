f2a7a29b3753c07ee4f88167a1ab51f8473ad7d1
Raniro Coelho <ranirocoelho@gmail.com>
2026-03-17 10:37:25 -0300

    test: cover setup question contract and shared choice enums

--- _gulp/gulpSetup.ts
+++ _gulp/gulpSetup.ts
@@ -3,6 +3,7 @@ import { writeFile } from 'fs/promises';
 import { copyVendorCSS, createProjectFiles, createProjectStructure } from './modules/fileSetup';
 import { generateGulpfile } from './modules/gulpfileGenerator';
 import { confirmProjectDeletion, promptUser } from './modules/setupQuestions';
+import { assertUserChoices } from './modules/userChoicesValidation';
 import { UserChoices } from './types';
 import { deleteDirectory, fileExists } from './utils/fileSystem';
 import { logger } from './utils/logger';
@@ -20,7 +21,8 @@ async function setup(): Promise<void> {
       deleteDirectory('dist');
     }
 
-    const choices: UserChoices = await promptUser();
+    const rawChoices = await promptUser();
+    const choices: UserChoices = assertUserChoices(rawChoices);
     
     await writeFile('_gulp/user-choices.json', JSON.stringify(choices, null, 2));
 
@@ -32,7 +34,7 @@ async function setup(): Promise<void> {
     logger.success('Setup complete. Gulpfile has been generated.');
     logger.info('Starting development server...');
 
-    exec('yarn start', (error, stdout, stderr) => {
+    exec('npm start', (error, stdout, stderr) => {
       if (error) {
         logger.error(`Error: ${error.message}`);
         return;
